7e8eee149adb2fb6a1c1bbe5a63807e223a13655
Filip Troníček <filip@gitpod.io>
2023-02-16 18:40:05 +0000

    Simplify building

--- rollup.config.js
+++ rollup.config.js
@@ -2,16 +2,18 @@ import merge from 'deepmerge';
 import commonjs from '@rollup/plugin-commonjs';
 import polyfillNode from 'rollup-plugin-polyfill-node';
 import { createBasicConfig } from '@open-wc/building-rollup';
+import typescript from '@rollup/plugin-typescript';
 
 const baseConfig = createBasicConfig();
 
 export default merge(baseConfig, {
-    input: './out-tsc/src/client.js',
+    input: './src/client.ts',
     treeshake: true,
     output: {
         file: 'dist/bundle.js',
         format: 'iife',
         dir: undefined,
+        sourcemap: 'inline',
         globals: {
           fs: 'require$$0',
           path: 'require$$1',
@@ -26,6 +28,9 @@ export default merge(baseConfig, {
             stream: 'stream-browserify'
           }
         }),
-        commonjs()
+        commonjs(),
+        typescript({
+          tsconfig: './tsconfig.json'
+        })
       ]
 });
--- server.cjs
+++ server.cjs
@@ -23,6 +23,7 @@ function startServer() {
   });
 
   app.use('/dist', express.static(__dirname + '/dist'));
+  app.use('/src', express.static(__dirname + '/src'));
 
   app.post('/terminals', (req, res) => {
     const env = Object.assign({}, process.env);
