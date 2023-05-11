gitpod-io/xterm-web-ide

commit c561fbe083a9d2fd89573c96a8ec1011b7949d1c

Filter out _that_ Device Attributes query

src/client.ts
@@ -155,6 +155,19 @@ async function createTerminal(element: HTMLElement, toDispose: DisposableCollect
         }
     });
 
+    let buffer = '';
+    term.onData((data) => {
+        buffer += data;
+
+        const unwantedSequence = '\x1b[0;276;0c';
+        if (buffer.includes(unwantedSequence)) {
+            buffer = buffer.replaceAll(unwantedSequence, '');
+        }
+
+        term.write(buffer);
+        buffer = '';
+    });
+
     toDispose.push(term);
 
     window.terminal = term;

commit dacb57468f587654410a9887c4f3c1248f53ac2c

Fix duplicated characters
This was a regression from c561fbe083a9d2fd89573c96a8ec1011b7949d1c

src/client.ts
@@ -162,9 +162,8 @@ async function createTerminal(element: HTMLElement, toDispose: DisposableCollect
         const unwantedSequence = '\x1b[0;276;0c';
         if (buffer.includes(unwantedSequence)) {
             buffer = buffer.replaceAll(unwantedSequence, '');
+            term.write(buffer);
         }
-
-        term.write(buffer);
         buffer = '';
     });
 